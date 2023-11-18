6c801eb818150bd1a9dc366deb93dd7b190d0c0c
Darin E. Reyes <hello@derey.es>
2023-11-18 19:40:14 +0000

    Move arrow into viz on mobile

--- components/layout/main/LayoutMain.vue
+++ components/layout/main/LayoutMain.vue
@@ -3,25 +3,22 @@
     <div class="layout-panel layout-panel-intro">
       <slot name="intro" />
     </div>
-    <div class="layout-arrow">
-      <div class="layout-arrow-inner">
-        <span class="layout-arrow-character">↓</span>
-      </div>
-    </div>
     <LayoutPanelViz />
     <div class="layout-panel layout-panel-skills">
       <slot name="skills" />
+    </div>
+    <div class="layout-panel layout-panel-footer">
+      <slot name="footer" />
     </div>
   </main>
 </template>
 
 <style lang="scss">
 .layout {
-  $panel-padding: u(0.5);
-
   &-main {
     display: grid;
     grid-auto-flow: row;
+    grid-template-rows: max-content;
     height: 100vh;
     overflow-y: scroll;
     position: relative;
@@ -42,45 +39,12 @@
     scroll-snap-align: start;
   }
 
-  &-arrow {
-    $arrow-size: u(3);
-    $arrow-inner-size: $arrow-size * 1.5;
-
-    $arrow-position-y: math.div($arrow-size, 2);
-
-    position: sticky;
-    top: $arrow-position-y + $panel-padding;
-    transform: translate(0, $arrow-position-y * -2);
-
-    &-inner {
-      background: color.palette("hilite");
-      border-radius: math.div($arrow-size, 2);
-      color: color.palette("concrete");
-      display: grid;
-      height: $arrow-size;
-      top: $arrow-position-y + $panel-padding;
-      place-items: center;
-      position: absolute;
-      right: $panel-padding;
-      width: $arrow-size;
-    }
-
-    &-character {
-      font-family: typography.fontFamily("sansSerif");
-    }
-  }
-
   @include breakpoint("max-mobile") {
     &-panel {
       &-intro {
-        min-height: 80vh;
+        min-height: 85vh;
       }
     }
-
-    // &-arrow {
-    //   grid-column: 1 / -1;
-    //   grid-row: 2 / 3;
-    // }
   }
 }
 </style>
