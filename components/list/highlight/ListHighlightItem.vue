eecb4a0e66a46a4c6d2b1b6e68212e0aebcb11e1
Darin E. Reyes <hello@derey.es>
2023-11-18 23:37:50 +0000

    Begin highlight components

---
+++ components/list/highlight/ListHighlightItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="list-highlight-item">
+    <slot />
+  </li>
+</template>
+
+<style lang="scss" scoped>
+.list-highlight-item {
+  display: flex;
+  flex-flow: column nowrap;
+  margin: 0 0 u(3);
+}
+</style>
