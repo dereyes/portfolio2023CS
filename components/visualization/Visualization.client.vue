08d6b37009fd60ef2b7df70e7ecafd983450216f
Darin E. Reyes <hello@derey.es>
2023-11-20 08:14:19 +0000

    p5 shader demo working

--- components/visualization/Visualization.client.vue
+++ components/visualization/Visualization.client.vue
@@ -11,92 +11,78 @@ const viz = ref(null);
 onMounted(() => {
   nextTick(() => {
     let visualization = (p5) => {
-      // let rectShader;
+      let rectShader;
 
       const canvasResized = () => {
         p5.resizeCanvas(viz.value.clientWidth, viz.value.clientHeight);
       };
 
       p5.preload = () => {
-        // rectShader = p5.loadShader("shader.vert", "shader.frag");
+        rectShader = p5.loadShader(
+          "shader.vert",
+          "shader.frag",
+          () => {
+            console.log("success?");
+          },
+          (error) => {
+            console.log(error);
+          },
+        );
       };
+
+      // the 'varying's are shared between both vertex & fragment shaders
+      let varying = "precision highp float; varying vec2 vPos;";
+
+      // the vertex shader is called for each vertex
+      let vs =
+        varying +
+        "attribute vec3 aPosition;" +
+        "void main() { vPos = (gl_Position = vec4(aPosition,1.0)).xy; }";
+
+      // the fragment shader is called for each pixel
+      let fs =
+        varying +
+        "uniform vec2 p;" +
+        "uniform float r;" +
+        "const int I = 500;" +
+        "void main() {" +
+        "  vec2 c = p + vPos * r, z = c;" +
+        "  float n = 0.0;" +
+        "  for (int i = I; i > 0; i --) {" +
+        "    if(z.x*z.x+z.y*z.y > 4.0) {" +
+        "      n = float(i)/float(I);" +
+        "      break;" +
+        "    }" +
+        "    z = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c;" +
+        "  }" +
+        "  gl_FragColor = vec4(0.5-cos(n*17.0)/2.0,0.5-cos(n*13.0)/2.0,0.5-cos(n*23.0)/2.0,1.0);" +
+        "}";
+
+      let mandel;
 
       p5.setup = () => {
-        // const canvas = p5.createCanvas(0, 0, p5.WEBGL);
-        const canvas = p5.createCanvas(0, 0, p5.P2D);
+        const canvas = p5.createCanvas(400, 400, p5.WEBGL);
         canvas.parent("viz");
-        p5.pixelDensity(1);
-        canvasResized();
-        p5.background(0);
-        p5.smooth();
-      };
-
-      let angle = 0;
-      let speed = 0.01;
-
-      const getPlanets = () => {
-        return Array.from({ length: 10 }, (_, index) => {
-          let radius = index * 25;
-          let angle = Math.random() * 5;
-          let speed = Math.random() * 2;
-
-          return {
-            radius: radius,
-            angle: angle,
-            speed: speed,
-          };
-        });
-      };
-
-      const pointPosition = (p5, angle, radius, speed) => {
-        const center = {
-          x: p5.width / 2,
-          y: p5.height / 2,
-        };
-
-        return {
-          x: center.x + radius * p5.cos(angle),
-          y: center.y + radius * p5.sin(angle),
-        };
-      };
-
-      const planets = getPlanets();
-
-      p5.draw = () => {
-        p5.blendMode(p5.MULTIPLY);
-        p5.background(0, 0.99);
-        p5.blendMode(p5.BLEND);
-
-        p5.stroke(170);
-        p5.strokeWeight(1);
-        p5.fill(0);
         // p5.shader(rectShader);
 
-        p5.point(p5.width / 2, p5.height / 2);
-        // p5.point(p5.sin(speed) * 9, p5.height / 2);
+        mandel = p5.createShader(vs, fs);
+        p5.shader(mandel);
 
-        // p5.point(
-        //   pointPosition(p5, angle, 50).x,
-        //   pointPosition(p5, angle, 50).y,
-        // );
+        p5.noStroke();
 
-        // p5.point(
-        //   pointPosition(p5, angle, 150).x,
-        //   pointPosition(p5, angle, 150).y,
-        // );
+        // 'p' is the center point of the Mandelbrot image
+        mandel.setUniform("p", [-0.74364388703, 0.13182590421]);
 
-        planets.forEach((planet) => {
-          p5.ellipse(
-            pointPosition(p5, angle + planet.angle, planet.radius, planet.speed)
-              .x,
-            pointPosition(p5, angle + planet.angle, planet.radius, planet.speed)
-              .y,
-            20,
-          );
-        });
+        // p5.rect(0, 0, p5.width, p5.height);
+      };
 
-        angle += speed;
-        // p5.rect(p5.mouseX - 300, p5.mouseY - 300, 100, 100);
+      p5.draw = () => {
+        // p5.rect(0, 0, p5.width, p5.height);
+        mandel.setUniform(
+          "r",
+          1.5 * p5.exp(-6.5 * (1 + p5.sin(p5.millis() / 2000))),
+        );
+        p5.quad(-1, -1, 1, -1, 1, 1, -1, 1);
       };
 
       p5.windowResized = () => {
--- public/shader.vert
+++ public/shader.vert
@@ -1,19 +1,18 @@
-attribute vec3 aPosition; 
-attribute vec2 aTexCoord; 
+// vert file and comments from adam ferriss
+// https://github.com/aferriss/p5jsShaderExamples
 
-void main() { 
+// our vertex data
+attribute vec3 aPosition;
 
-// Copy the position data into a vec4, using 
-// 1.0 as the w component 
-vec4 positionVec4 = vec4(aPosition, 1.0); 
+// our texcoordinates
+attribute vec2 aTexCoord;
 
-// scale the rect by two, and move it to the 
-// center of the screen if we don't do this, 
-// it will appear with its bottom left corner 
-// in the center of the sketch try commenting 
-// this line out to see what happens 
-positionVec4.xy = positionVec4.xy * 2.0 - 1.0; 
+void main() {
 
-// Send the vertex information on to the fragment shader 
-gl_Position = positionVec4; 
+  // copy the position data into a vec4, using 1.0 as the w component
+  vec4 positionVec4 = vec4(aPosition, 1.0);
+  positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+
+  // send the vertex information on to the fragment shader
+  gl_Position = positionVec4;
 }
