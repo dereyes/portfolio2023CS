f7bee62866ffe1bba2c74484bd67c33f174d2b70
Darin Reyes <hello@derey.es>
2023-11-25 18:54:21 -0800

    Testing mobile scroll fix

--- components/layout/main/LayoutMain.vue
+++ components/layout/main/LayoutMain.vue
@@ -10,16 +10,12 @@
   display: flex;
   flex-flow: column nowrap;
   gap: 25vh;
-  gap: 25dvh; // Better on mobile, falls back to vh
   margin: 0 auto;
   width: 100%;
 
-  @include breakpoint(
-    (
-      "start": "tablet",
+  @include breakpoint(("start": "tablet",
       "end": null,
-    )
-  ) {
+    )) {
     max-width: getBreakpoint("max");
   }
 }
