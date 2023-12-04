d6d8f2c7fea04034ad0284d805728eacc6b70ccb
Darin Reyes <hello@derey.es>
2023-12-03 21:14:42 -0800

    Start implementing noise

--- components/background/Background.vue
+++ components/background/Background.vue
@@ -1,19 +1,14 @@
 
 <template>
-  <TresCanvas window-size clear-color="blue">
+  <TresCanvas window-size clear-color="#222">
     <TresPerspectiveCamera :position="[11, 11, 11]" />
     <OrbitControls />
-    <TresMesh ref="blobRef" :position="[0, 4, 0]">
-      <TresSphereGeometry />
-      <!-- <TresMeshNormalMaterial /> -->
-      <TresShaderMaterial ref="materialRef" :vertex-shader="vertexShader" :fragment-shader="fragmentShader"
-        :uniforms="uniforms" />
-    </TresMesh>
-    <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
+    <TresMesh ref="blobRef" :rotation="[-Math.PI / 2, 0, 0]">
       <TresPlaneGeometry :args="[10, 10, 10, 10]" />
-      <TresMeshBasicMaterial color="#444" />
+      <TresShaderMaterial :vertex-shader="vertexShader" :fragment-shader="fragmentShader" :uniforms="uniforms"
+        wireframe />
     </TresMesh>
-    <TresGridHelper />
+    <!-- <TresGridHelper /> -->
   </TresCanvas>
 </template>
 
@@ -24,7 +19,7 @@ import vertexShader from "./shaders/vertex.glsl?raw";
 import fragmentShader from './shaders/fragment.glsl?raw';
 
 const blobRef = ref(null);
-const materialRef = ref(null);
+// const materialRef = ref(null);
 const uniforms = {
   uTime: { value: 0 },
   uAmplitude: { value: new Vector2(0.1, 0.1) },
--- components/background/shaders/fragment.glsl
+++ components/background/shaders/fragment.glsl
@@ -2,6 +2,9 @@
 precision mediump float;
 varying vec2 vUv;
 
+varying vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1.0, vUv.y, 0.5, 1.0);
+  gl_FragColor = vec4(vUv, 0.0, 1.);
+  // gl_FragColor = vec4(vColor, 1.0);
 }
