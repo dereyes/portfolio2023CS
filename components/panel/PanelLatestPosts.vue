9438167e550e6b596ae6e44e0030ce0af75d0af7
Darin Reyes <hello@derey.es>
2023-12-06 17:56:53 -0800

    Start latestPosts panel

--- components/panel/PanelLatestPosts.vue
+++ components/panel/PanelLatestPosts.vue
@@ -3,9 +3,15 @@
     <Panel autoHeight class="panel-latestPosts">
       <h2>Latest Posts</h2>
       <div class="panel-latestPosts-carousel">
-        <h3>Fluid Typography in SASS</h3>
-        <h3>Generating Typescales in SASS</h3>
-        <h3>Generating Typescales in SASS</h3>
+        <a class="panel-latestPosts-carousel-item" href="#">
+          <p>Fluid Typography in SASS</p>
+        </a>
+        <a class="panel-latestPosts-carousel-item" href="#">
+          <p>Generating Harmonic Typescales in SASS</p>
+        </a>
+        <a class="panel-latestPosts-carousel-item" href="#">
+          <p>"Good design is as little design as possible": Thoughts on Intrinsic Design</p>
+        </a>
       </div>
     </Panel>
   </Panel>
@@ -14,8 +20,16 @@
 <style lang="scss" scoped>
 .panel-latestPosts {
   &-carousel {
+    @include responsiveGap;
+
     display: flex;
     flex-flow: row;
+
+    &-item {
+      @include borderTop;
+
+      flex: 1 0;
+    }
   }
 }
 </style>
