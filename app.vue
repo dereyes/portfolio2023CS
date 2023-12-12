4fac4df96f32d9ef48dea832780f58d9eb5c58ed
Darin Reyes <hello@derey.es>
2023-12-11 22:26:00 -0800

    Toggle body classes on nav toggle

--- app.vue
+++ app.vue
@@ -1,35 +1,66 @@
 <template>
   <div class="app">
     <NuxtLayout>
-      <NuxtPage
-        :transition="{
-          name: 'slide-right',
-          mode: 'out-in',
-        }"
-      />
+      <NuxtPage :transition="{
+        name: 'slide-right',
+        mode: 'out-in',
+      }" />
     </NuxtLayout>
   </div>
 </template>
 
+<script setup>
+import { useNavStore } from "@/stores/navStore";
+
+const navStore = useNavStore();
+
+const classObject = computed(() => ({
+  "body-navIsClosed": !navStore.isOpen,
+  "body-navIsOpened": navStore.isOpen
+}));
+
+useHead({
+  bodyAttrs: {
+    class: classObject
+  },
+})
+</script>
+
 <style lang="scss">
+.body {
+  &-navIsClosed {
+    overflow-x: hidden;
+    overflow-y: scroll;
+  }
+
+  &-navIsOpened {
+    overflow-x: hidden;
+    overflow-y: clip;
+  }
+}
+
 .slide-left-enter-active,
 .slide-left-leave-active,
 .slide-right-enter-active,
 .slide-right-leave-active {
   transition: all 0.2s;
 }
+
 .slide-left-enter-from {
   opacity: 0;
   transform: translate(50px, 0);
 }
+
 .slide-left-leave-to {
   opacity: 0;
   transform: translate(-50px, 0);
 }
+
 .slide-right-enter-from {
   opacity: 0;
   transform: translate(-50px, 0);
 }
+
 .slide-right-leave-to {
   opacity: 0;
   transform: translate(50px, 0);
