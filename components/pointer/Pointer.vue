82dc76caf0a8c6d3284465821988aab8710c9627
Darin Reyes <hello@derey.es>
2023-12-17 16:22:16 -0800

    Easing working, before requestAnimationFrame

--- components/pointer/Pointer.vue
+++ components/pointer/Pointer.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="pointer-area" ref="pointerArea">
-    <div class="pointer-follower" ref="pointerFollower" :style="styleObject"></div>
+  <div class="pointer-area">
+    <div class="pointer-follower-origin" ref="pointerFollowerOrigin" :style="styleObject">
+      <div class="pointer-follower"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
 
-const pointerArea = ref();
-const pointerFollower = ref();
+const pointerSpeed = .1;
 const pointerPosition = ref({ x: 0, y: 0 });
+const pointerTarget = ref({ x: 0, y: 0 });
+const pointerDistance = ref({ x: 0, y: 0 });
+const pointerFollowerOrigin = ref();
 
 const styleObject = computed(() => ({
   transform: `translate(${pointerPosition.value.x}px, ${pointerPosition.value.y}px)`,
@@ -17,10 +21,19 @@ const styleObject = computed(() => ({
 
 onMounted(() => {
   const onMousemove = (event) => {
-    console.log(event);
-    pointerPosition.value = {
+    pointerTarget.value = {
       x: event.x,
-      y: event.y
+      y: event.y,
+    }
+
+    pointerDistance.value = {
+      x: event.x - pointerPosition.value.x,
+      y: event.y - pointerPosition.value.y,
+    }
+
+    pointerPosition.value = {
+      x: pointerPosition.value.x + (pointerDistance.value.x * pointerSpeed),
+      y: pointerPosition.value.y + (pointerDistance.value.y * pointerSpeed),
     }
   }
 
@@ -31,7 +44,6 @@ onMounted(() => {
 <style lang="scss" scoped>
 .pointer {
   &-area {
-    border: 1px solid red;
     height: 100%;
     left: 0;
     position: fixed;
@@ -50,6 +62,11 @@ onMounted(() => {
     position: absolute;
     top: -$follower-offset;
     width: u(3);
+
+    &-origin {
+      height: 0;
+      width: 0;
+    }
   }
 }
 </style>
