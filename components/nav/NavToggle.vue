fb0654038993b6a286a97125ad52d681c4148769
Darin Reyes <hello@derey.es>
2023-12-18 07:42:44 -0800

    Save nav toggle coordinates to store

--- components/nav/NavToggle.vue
+++ components/nav/NavToggle.vue
@@ -1,12 +1,31 @@
 <template>
-  <button class="nav-toggle" @click="navStore.toggle()">
+  <button class="nav-toggle" @click="navStore.toggle()" ref="navToggle">
     <span class="nav-toggle-icon">+</span>
   </button>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import { useNavStore } from "@/stores/navStore";
+
 const navStore = useNavStore();
+const navToggle = ref();
+
+const getNavToggleCenterCoordinates = () => {
+  const rect = navToggle.value.getBoundingClientRect();
+
+  const rectCenterCoordinates = {
+    x: rect.left + (rect.width / 2),
+    y: rect.top + (rect.height / 2),
+  };
+
+  navStore.setNavToggleCenterCoordinates(rectCenterCoordinates);
+}
+
+onMounted(() => {
+  getNavToggleCenterCoordinates();
+});
+
 </script>
 
 <style lang="scss" scoped>
