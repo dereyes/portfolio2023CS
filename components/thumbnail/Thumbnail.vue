59c574e85aa02f20a7a1b06b53487f19d003ea21
Darin Reyes <hello@derey.es>
2024-02-26 21:30:44 -0800

    Create thumbnail component

---
+++ components/thumbnail/Thumbnail.vue
@@ -0,0 +1,23 @@
+<template>
+  <a class="thumbnail" :href="link" target="_blank">
+    <img :src="source" width="300" height="150">
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  link: String,
+  source: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.thumbnail {
+  filter: grayscale(100%);
+  transition: filter 0.25s ease;
+
+  &:hover {
+    filter: grayscale(0%);
+  }
+}
+</style>
